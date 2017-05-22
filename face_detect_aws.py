1d5bcc20804f1188592be69b34ec912578e90e0d
Sunny <gsunny4178@gmail.com>
2017-05-22 12:54:34 +0800

    initial commit

---
+++ face_detect_aws.py
@@ -0,0 +1,21 @@
+import boto3
+
+client = boto3.client(
+    'rekognition',
+    aws_access_key_id='YOUR_ACCESS_KEY',
+    aws_secret_access_key='YOUR_SECRET_ACCESS_KEY',
+    region_name='YOUR_REGION_NAME'
+)
+
+p = open("f1.jpg", 'rb')
+
+face_features = client.detect_faces(Image={
+    'Bytes': bytearray(p.read())
+},
+    Attributes=[
+        'ALL'
+    ])
+
+p.close()
+
+print face_features
